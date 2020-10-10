9fe7385334106abb7e30a10f90f26b3652b44f12
BHessel <hesselfitness@gmail.com>
2020-10-10 13:06:57 -0400

    lab complete

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+    def initialize(name=nil, breed=nil)
+        @name = name
+        @breed = breed
+    end
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(input_breed)
+        @breed = input_breed
+    end
+end
+    
+
+
+    
